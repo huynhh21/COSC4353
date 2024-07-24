641e9cecab9845a2300e829863a3d6d42e0dfe01
Elijah-24 <145415614+Elijah-24@users.noreply.github.com>
2024-07-24 14:30:57 -0500

    Update UpdateEvent.js

--- src/components/EventManagement/UpdateEvent.js
+++ src/components/EventManagement/UpdateEvent.js
@@ -2,12 +2,13 @@ import axios from 'axios';
 import React, { useState } from 'react'
 import { useNavigate, useParams } from 'react-router-dom';
 
-// Frontend for updating an event 
+// Added to frontend GitHub
 
 function UpdateEvent() {
     const [name, setName] = useState('')
     const [description, setDescription] = useState('')
     const [location, setLocation] = useState('')
+    const [requiredSkills, setRequiredSkills] = useState('')
     const [urgency, setUrgency] = useState(0)
     const [date, setDate] = useState(new Date());
     const {id} = useParams();
@@ -16,7 +17,7 @@ function UpdateEvent() {
     function handleSubmit(event) {
         event.preventDefault();
 
-        axios.put('http://localhost:8081/update/'+id, {name, description, location, urgency, date})
+        axios.put('http://localhost:8081/update/'+id, {name, description, location, requiredSkills, urgency, date})
         .then(res => {
             console.log(res);
             navigate('/');
@@ -30,31 +31,37 @@ function UpdateEvent() {
                 <h2>Update Event</h2>
                 <div className='mb-2'>
                     <label htmlFor="">Name</label>
-                    <input type="text" placeholder = 'Enter name' className='form-control'
+                    <input type="Name" placeholder = 'Enter name' className='form-control'
                     onChange={e => setName(e.target.value)}
                     />
                 </div>
                 <div className='mb-2'>
                     <label htmlFor="">Description</label>
-                    <input type="description" placeholder='Enter description' className='form-control'
+                    <input type="Description" placeholder='Enter description' className='form-control'
                     onChange={e => setDescription(e.target.value)}
                     />
                 </div>
                 <div className='mb-2'>
                     <label htmlFor="">Location</label>
-                    <input type="Location" placeholder='Enter Location' className='form-control'
+                    <input type="Location" placeholder='Enter location' className='form-control'
                     onChange={e => setLocation(e.target.value)}
                     />
                 </div>
                 <div className='mb-2'>
+                    <label htmlFor="">Required Skills</label>
+                    <input type="Required Sklls" placeholder='Enter required sklls' className='form-control'
+                    onChange={e => setRequiredSkills(e.target.value)}
+                    />
+                </div>
+                <div className='mb-2'>
                     <label htmlFor="">Urgency</label>
-                    <input type="Urgency" placeholder='Enter Urgency' className='form-control'
+                    <input type="Urgency" placeholder='Enter urgency' className='form-control'
                     onChange={e => setUrgency(e.target.value)}
                     />
                 </div>
                 <div className='mb-2'>
                     <label htmlFor="">Date</label>
-                    <input type="Date" placeholder='Enter Date' className='form-control'
+                    <input type="Date" placeholder='Enter date' className='form-control'
                     onChange={e => setDate(e.target.value)}
                     />
                 </div>
